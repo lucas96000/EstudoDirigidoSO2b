bdc47646eea053c526498f44abbeef9b93a62df6
Lucas Ferreira de Almeida <37914656+lucaspeta@users.noreply.github.com>
2019-05-23 21:37:39 -0300

    comentarios e mudanca no nome do deque

--- src/circular.h
+++ src/circular.h
@@ -4,6 +4,8 @@
 #include <deque>
 #include <algorithm>
 
+#define QUANTUM 2
+
 class Circular: public Escalonador{
 	public:
 		Circular(){
@@ -16,61 +18,74 @@ class Circular: public Escalonador{
 			int timeline = 0, q = 0;
 			
 			// Map parametros
-			// 1- string ID
+			// 1- string processoID
 			// 2- int tempo de processo 
 			map<string, int> processos;
 			
-			deque<string> fila;
+			// DequeAkaPilha
+			// 1 - string processoID
+			deque<string> pilha;
 			
+			// Inseri os processos no map
 			for(int i = 0; i < Escalonador::getContador(); ++i){
 				processos[Escalonador::getProcesso(i)->getID()] = Escalonador::getProcesso(i)->getTempoCPU();				
 			}
 			
+			// Enquanto houver processos pendentes
 			while(processos.size() > 0){
 				for(int i = 0; i < Escalonador::getContador(); ++i){
-					if(Escalonador::getProcesso(i)->getTempoC() == timeline){				
-						if(q >= 0 && q < 2){
-							fila.push_back(Escalonador::getProcesso(i)->getID());
+					// se o tempo de chegada for igual ao da timeline
+					if(Escalonador::getProcesso(i)->getTempoC() == timeline){	
+						/* * * * EVENTO ESPECIAL
+						*
+						* Descricao do evento: se na mesma hora que o quantum acabar e um processo chega,
+						* eh necessario que o processo que chegou fique na frente em relacao ao processo
+						* que estava em execucao pilha.
+						*
+						*/
+						if(q >= 0 && q < QUANTUM){
+							pilha.push_back(Escalonador::getProcesso(i)->getID());
 						}
 						else{
-							if(fila.size() == 0){
-								fila.push_back(Escalonador::getProcesso(i)->getID());
+							if(pilha.size() == 0){
+								pilha.push_back(Escalonador::getProcesso(i)->getID());
 							}
 							else{
-								string _aux = fila.back();
-								fila.pop_back();							
+								string _aux = pilha.back();
+								pilha.pop_back();							
 								
-								fila.push_back(Escalonador::getProcesso(i)->getID());
-								fila.push_back(_aux);
+								pilha.push_back(Escalonador::getProcesso(i)->getID());
+								pilha.push_back(_aux);
 							} 							
 						}							
 					}							
 				}					
 					
 					
-				if(fila.size() == 0){
+				if(pilha.size() == 0){
 					cout << timeline << " -> --" << endl;
 				}			
 				else{
 					//Executa o processo	
-					cout << timeline << " -> " << fila.front() << endl;				
+					cout << timeline << " -> " << pilha.front() << endl;				
 	
-					if(processos.find(fila.front())->second > 1){				
-						processos[fila.front()] = processos.find(fila.front())->second - 1;			
+					if(processos.find(pilha.front())->second > 1){				
+						processos[pilha.front()] = processos.find(pilha.front())->second - 1;			
 						q++;					
 					}
 					//processo finalizado
 					else{
-						processos.erase(fila.front());
-						fila.pop_front();
+						processos.erase(pilha.front());
+						pilha.pop_front();
 						q = 0;
 					}			
 					
-					if(q == 2){
-						string aux = fila.front();						
-						fila.pop_front();
+					// QUANTUM
+					if(q == QUANTUM){
+						string aux = pilha.front();						
+						pilha.pop_front();
 						
-						fila.push_back(aux);
+						pilha.push_back(aux);
 						q = 0;
 					}
 				}			
